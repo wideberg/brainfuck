553048f295e9a60d6a1272795528a0841436bc1b
Magnus Wideberg <magnus@wideberg.com>
2016-04-17 20:59:23 +0200

    Fixed a bunch of warnings in Program

--- Program.hs
+++ Program.hs
@@ -10,13 +10,14 @@ new s = Program [] s
 
 current :: Program -> Char
 current (Program _ []) = error "end of program"
-current (Program _ (p:ps)) = p
+current (Program _ (p:_)) = p
 
 next :: Program -> Program
-next (Program beginning (x:ending)) = Program (x:beginning) ending
+next (Program beg (x:end)) = Program (x:beg) end
+next (Program _ []) = error "can't move past end of program"
 
 prev :: Program -> Program
-prev (Program (x:beginning) ending) = Program beginning (x:ending)
+prev (Program (x:beg) end) = Program beg (x:end)
 prev (Program [] ps) = error $ "prev Unexpected: " ++ ps
 
 matchingRightBracket :: Program -> Program
@@ -26,12 +27,7 @@ matchingRightBracket prog = b' 0 (next prog) where
   b' d p  | (current p) == ']' = b' (d-1) (next p)
           | (current p) == '[' = b' (d+1) (next p)
           | otherwise = b' d (next p)
-  b' _ p = error $ "foo: " ++ (show p)
 
---matchingLeftBracket :: Program -> Program
---matchingLeftBracket p = b' 0 p where
---  b' :: Integer -> Program -> Program
---  b' 0
 matchingLeftBracket :: Program -> Program
 matchingLeftBracket program = b' 0 (prev program) where
   b' :: Int -> Program -> Program
